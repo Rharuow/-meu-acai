40a0f0097b0bd6e58cac0caa646db24ce200d1ea
Harysson <haryssonsoares@gmail.com>
2024-01-25 20:51:31 -0300

    wip: sign in page and validation user cokkie session to home page

--- frontend/admin/src/app/page.tsx
+++ frontend/admin/src/app/page.tsx
@@ -1,3 +1,15 @@
+import { Dashboard } from "@/components/domain/home/dashboard";
+import { SignIn } from "@/components/domain/home/signIn";
+import { cn } from "@/lib/utils";
+import { getCookie } from "cookies-next";
+
 export default function Home() {
-  return <main className=""></main>;
+  const user = getCookie("user");
+  return (
+    <main
+      className={cn({ "min-h-screen flex justify-center items-center": !user })}
+    >
+      {user ? <Dashboard /> : <SignIn />}
+    </main>
+  );
 }
