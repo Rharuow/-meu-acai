553ea3b86da04a2765566f749033357d605ed9fc
Harysson <haryssonsoares@gmail.com>
2024-01-26 20:14:24 -0300

    improvement: add back navigation button to not found page

--- frontend/admin/src/app/not-found.tsx
+++ frontend/admin/src/app/not-found.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Lottie from "lottie-react";
+import { ChevronLeft } from "lucide-react";
 
 import { Card } from "@/components/ui/card";
 
@@ -7,16 +8,27 @@ import notFound from "@public/animations/404.json";
 
 import t from "@/util/translate.json";
 import { Image } from "@/components/ui/image";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-3">
+    <main className="relative flex min-h-screen flex-col items-center justify-center gap-3">
+      <Button
+        variant="secondary"
+        className="absolute left-3 top-3 rounded-full p-2"
+        onClick={() => router.back()}
+      >
+        <ChevronLeft />
+      </Button>
       <Image
         className="h-auto w-[200px] object-contain"
         alt="logo of market place"
         src="/logo.png"
       />
-      <Card className="bg-[var(--secondary-active)] p-3">
+      <Card className="bg-[var(--secondary-active)] p-3 ">
         <Lottie animationData={notFound} className="h-[150px]" />
         <p className="text-center font-bold text-muted">
           {t["pt-BR"].pages[404]["Ops... Not found here..."]}
